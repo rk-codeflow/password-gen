44a72c94d34330d9339b782be698faa704fcca3f
Raj Kiran Chaudhary <raj.chaudhary@ebpearls.com>
2023-11-06 17:32:34 +0545

    password generator in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [range, setRange] = useState(8);
+  const [allowNum, setAllowNum] = useState(false);
+  const [allowChars, setAllowChars] = useState(false);
+
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let str = "";
+
+  const randomVal = () => {
+    for (let i = 0; i < chars; i++) {
+      let randomChars = Math.floor(Math.random() * chars.length);
+    }
+    console.log(randomChars);
+  };
+
+  const rangeVal = (e) => {
+    const newValue = e.target.value;
+    setRange(newValue);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h2 className="text-center text-white mt-3 text-2xl">
+        Random Password Generator
+      </h2>
+      <div className="mt-10 max-w-3xl mx-auto bg-slate-500 p-4 rounded-lg">
+        <div className="flex">
+          <input
+            type="text"
+            className="w-full h-10 outline-none rounded-l-md p-1"
+          />
+          <button className="bg-blue-500 py-1 px-3 text-white rounded-r-md">
+            Copy
+          </button>
+        </div>
+        <div className="flex gap-x-4 justify-center mt-3">
+          <div className="flex gap-x-2">
+            <input
+              type="range"
+              min="8"
+              max="100"
+              onChange={rangeVal}
+              value={range}
+            />
+            <label>Length: {range}</label>
+          </div>
+          <div className="flex gap-x-2">
+            <input type="checkbox" />
+            <label>Numbers</label>
+          </div>
+          <div className="flex gap-x-2">
+            <input type="checkbox" />
+            <label>Characters</label>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
