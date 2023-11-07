13c61a1a06f8fe57e693860eb1b68b629d650d23
Raj Kiran Chaudhary <raj.chaudhary@ebpearls.com>
2023-11-07 13:29:57 +0545

    random string character generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [range, setRange] = useState(8);
   const [allowNum, setAllowNum] = useState(false);
   const [allowChars, setAllowChars] = useState(false);
+  const [password, setPassword] = useState("");
 
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   let str = "";
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const num = "0123456789";
+  const specialChars = "!@#$%^&*()~`";
 
-  const randomVal = () => {
-    for (let i = 0; i < chars; i++) {
-      let randomChars = Math.floor(Math.random() * chars.length);
+  const passwordGenerator = () => {
+    // Generate random value for generating random chars
+    for (let i = 1; i <= range; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      const randomChar = chars[randomIndex];
+      str += randomChar;
     }
-    console.log(randomChars);
+    console.log("Random", str, range);
+    setPassword(str);
+
+    // numbers
   };
 
-  const rangeVal = (e) => {
-    const newValue = e.target.value;
-    setRange(newValue);
-  };
+  useEffect(() => {
+    passwordGenerator();
+  }, [range, allowNum, allowChars]);
 
   return (
     <div>
@@ -28,6 +36,7 @@ function App() {
       <div className="mt-10 max-w-3xl mx-auto bg-slate-500 p-4 rounded-lg">
         <div className="flex">
           <input
+            value={password}
             type="text"
             className="w-full h-10 outline-none rounded-l-md p-1"
           />
@@ -41,17 +50,31 @@ function App() {
               type="range"
               min="8"
               max="100"
-              onChange={rangeVal}
+              onChange={(e) => {
+                setRange(e.target.value);
+              }}
               value={range}
             />
             <label>Length: {range}</label>
           </div>
           <div className="flex gap-x-2">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              onChange={() => {
+                setAllowNum((prev) => !prev);
+              }}
+              defaultChecked={allowNum}
+            />
             <label>Numbers</label>
           </div>
           <div className="flex gap-x-2">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              onChange={() => {
+                setAllowChars((prev) => !prev);
+              }}
+              defaultChecked={allowChars}
+            />
             <label>Characters</label>
           </div>
         </div>
