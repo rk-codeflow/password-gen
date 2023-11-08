eee94fe7b75d7299761e4cc6cb9e126a290a9371
Raj Kiran Chaudhary <raj.chaudhary@ebpearls.com>
2023-11-08 18:12:22 +0545

    number, characters and copy to clipboard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   const [range, setRange] = useState(8);
   const [allowNum, setAllowNum] = useState(false);
   const [allowChars, setAllowChars] = useState(false);
   const [password, setPassword] = useState("");
-
-  let str = "";
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  const num = "0123456789";
-  const specialChars = "!@#$%^&*()~`";
+  const [buttonText, setButtonText] = useState("Copy");
+  const inputRef = useRef(null);
 
   const passwordGenerator = () => {
+    let str = "";
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (allowNum) {
+      chars += "0123456789";
+    }
+
+    if (allowChars) {
+      chars += "!@#$%^&*";
+    }
+
     // Generate random value for generating random chars
     for (let i = 1; i <= range; i++) {
       const randomIndex = Math.floor(Math.random() * chars.length);
-      const randomChar = chars[randomIndex];
-      str += randomChar;
+      // const randomChar = chars[randomIndex];
+      str += chars[randomIndex];
     }
-    console.log("Random", str, range);
-    setPassword(str);
 
-    // numbers
+    console.log("Random", str, range);
+
+    setPassword(str);
+  };
+
+  const copyToClipboard = () => {
+    if (inputRef.current) {
+      inputRef.current.select();
+      document.execCommand("copy");
+      setButtonText("Copied");
+    }
   };
 
   useEffect(() => {
@@ -36,12 +52,17 @@ function App() {
       <div className="mt-10 max-w-3xl mx-auto bg-slate-500 p-4 rounded-lg">
         <div className="flex">
           <input
+            ref={inputRef}
             value={password}
             type="text"
+            readOnly
             className="w-full h-10 outline-none rounded-l-md p-1"
           />
-          <button className="bg-blue-500 py-1 px-3 text-white rounded-r-md">
-            Copy
+          <button
+            className="bg-blue-500 py-1 px-3 text-white rounded-r-md"
+            onClick={copyToClipboard}
+          >
+            {buttonText}
           </button>
         </div>
         <div className="flex gap-x-4 justify-center mt-3">
